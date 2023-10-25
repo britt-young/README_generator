326c051ab5f884c48e1c3a0bcd7952906545a241
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-10-25 11:49:38 -0400

    refactor
    restructured prompt questions and included validate

--- Develop/index.js
+++ Develop/index.js
@@ -3,76 +3,146 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-      type: "title",
-      message: "Please provide a title for your README file",
-      title: "title",
+const questions = inquirer.prompt([
+  {
+    type: "input",
+    message: "Please provide a title for your README file",
+    validate: function (input) {
+      return input !== '' ? true : 'Title cannot be empty';
     },
-    {
-      type: "description",
-      message: "Please provide a short description.",
-      text: "description",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please provide a short description.",
+    validate: function (input) {
+      return input !== '' ? true : 'Description cannot be empty';
     },
-    {
-      type: "table of contents",
-      message: "Please select what you want to include in your README table of contents (optional).",
-      choices: [
-        "Installation",
-        "Usage",
-        "License",
-        "Contributions",
-        "Test",
-        "None"
-      ]
+    name: "description",
+  },
+  {
+    type: "checkbox",
+    message:
+      "Please select what you want to include in your README table of contents (optional).",
+      validate: function (input) {
+        return input !== '' ? true : 'Select "none" if you do not wish to include a Table of Contents';
+      },
+    name: "table of contents",
+    choices: [
+      "Installation",
+      "Usage",
+      "License",
+      "Contributions",
+      "Test",
+      "None"
+    ]
+  },
+  {
+    type: "input",
+    message: "Please provide intallation instructions needed.",
+    validate: function (input) {
+      return input !== '' ? true : 'Installation cannot be empty';
     },
-    {
-      type: "installation",
-      message: "Please provide intallation instructions needed.",
-      install: "install",
+    name: "install",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide relevant usage information including examples and/or screenshots.",
+      validate: function (input) {
+        return input !== '' ? true : 'Usage cannot be empty';
+      },
+    name: "usage",
+  },
+  {
+    type: "checkbox",
+    message: "What licensing is being used?",
+    validate: function (input) {
+      return input !== '' ? true : 'Select "none" if you do not wish to include a license';
     },
-    {
-      type: "usage",
-      message: "Please provide relevant usage information including examples and/or screenshots.",
-      usage: "usage",
+    name: "licensing",
+    choices: [
+      "MIT License",
+      "GNU General Public License v3",
+      "None"
+    ]
+  },
+  {
+    type: "input",
+    message:
+      "Please include all contributors and contribution guidelines to include GitHub profiles and all third-party assets.",
+      validate: function (input) {
+        return input !== '' ? true : 'Contribution cannot be empty';
+      },  
+    name: "contributions",
+  },
+  {
+    type: "input",
+    message:
+      "Provide examples on how to run your test code and the expected results.",
+      validate: function (input) {
+        return input !== '' ? true : 'Test cannot be empty';
+      },
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "Please enter your GitHub username",
+    validate: function (input) {
+      return input !== '' ? true : 'Must provide username';
     },
-    {
-      type: "license",
-      message: "What licensing is being used?",
-      choices: [
-        "MIT License",
-        "GNU General Public License v3",
-        "None"
-      ]
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Please enter your email address",
+    validate: function (input) {
+      return input !== '' ? true : 'Email cannot be empty';
     },
-    {
-      type: "contributions",
-      message: "Please include all contributors and contribution guidelines to include GitHub profiles and all third-party assets.",
-      contributions: "contributions",
-    },
-    {
-      type: "tests",
-      message: "Provide examples on how to run your test code and the expected results.",
-      tests: "tests",
-    },
-    {
-      type: "questions",
-      message: "Please enter your GitHub username",
-      username: "username",
-    },
-    {
-      type: "questions",
-      message: "Please enter your email address",
-      email: "email",
-    },
-  ])
-//replace "content"
-  .then((data) => {
-      fs.writeFile("file name here", content, (err)=>err?console.error(err):console.log("success!")),
-  });
+    name: "email",
+  },
+]);
+inquirer.prompt(questions).then((answers) => {
+  // Construct the README content using the user's inputs
+  const readmeContent = `
+  # ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  ${answers.tableOfContents.join("\n")}
+  
+  ## Installation
+  ${answers.installation || "No installation instructions provided."}
+  
+  ## Usage
+  ${answers.usage || "No usage information provided."}
+  
+  ## License
+  ${answers.license || "No license information provided."}
+  
+  ## Contributions
+  ${answers.contributions || "No contribution information provided."}
+  
+  ## Tests
+  ${answers.tests || "No test information provided."}
+  
+  ## Questions
+  GitHub: [${answers.username}](https://github.com/${answers.username})
+  Email: ${answers.email}
+    `;
 
-//log.txt file to store data????
+  fs.writeFile("README.md", readmeContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README created successfully!");
+    }
+  });
+});
+
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
