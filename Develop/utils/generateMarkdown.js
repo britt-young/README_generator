352e35eed32beaab55e00061a26d8fd52e5e6d86
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-10-25 17:57:20 -0400

    add
    generateMarkdown code for licensing

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,39 @@
 //TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT License") {
+    return "[![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "GNU General Public License v3") {
+    return "[![GPLv3 License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else {
+    // If there is no license, return an empty string
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT License") {
+    return "(https://opensource.org/licenses/MIT)";
+  } else if (license === "GNU General Public License v3") {
+    return "(https://www.gnu.org/licenses/gpl-3.0)";
+  } else {
+    // If there is no license, return an empty string
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+function renderLicenseSection(license) {
+  if (license === "MIT License") {
+    return "This project is licensed under the MIT License - see the [MIT License](https://opensource.org/licenses/MIT) for details.";
+  } else if (license === "GNU General Public License v3") {
+    return "This project is licensed under the GNU General Public License v3 - see the [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0) for details.";
+  } else {
+   // If there is no license, return an empty string
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
