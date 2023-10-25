4516b7c590c4a4aa03927364d9ff0d2b3db26eed
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-10-25 17:45:06 -0400

    refactor
    restructured array by removing incorrect usage of prompt keyword

--- Develop/index.js
+++ Develop/index.js
@@ -3,31 +3,33 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+const questions = [
   {
     type: "input",
-    message: "Please provide a title for your README file",
+    name: "title",
+    message: "Please provide a title for your README file.",
     validate: function (input) {
       return input !== '' ? true : 'Title cannot be empty';
     },
-    name: "title",
+  
   },
   {
     type: "input",
+    name: "description",
     message: "Please provide a short description.",
     validate: function (input) {
       return input !== '' ? true : 'Description cannot be empty';
     },
-    name: "description",
+    
   },
   {
     type: "checkbox",
+    name: "tableOfContents",
     message:
       "Please select what you want to include in your README table of contents (optional).",
       validate: function (input) {
         return input !== '' ? true : 'Select "none" if you do not wish to include a Table of Contents';
       },
-    name: "tableOfContents",
     choices: [
       "Installation",
       "Usage",
@@ -39,28 +41,28 @@ const questions = inquirer.prompt([
   },
   {
     type: "input",
+    name: "installation",
     message: "Please provide intallation instructions needed.",
     validate: function (input) {
       return input !== '' ? true : 'Installation cannot be empty';
     },
-    name: "installation",
   },
   {
     type: "input",
+    name: "usage",
     message:
       "Please provide relevant usage information including examples and/or screenshots.",
       validate: function (input) {
         return input !== '' ? true : 'Usage cannot be empty';
       },
-    name: "usage",
   },
   {
     type: "checkbox",
+    name: "license",
     message: "What licensing is being used?",
     validate: function (input) {
       return input !== '' ? true : 'Select "none" if you do not wish to include a license';
     },
-    name: "license",
     choices: [
       "MIT License",
       "GNU General Public License v3",
@@ -69,39 +71,39 @@ const questions = inquirer.prompt([
   },
   {
     type: "input",
+    name: "contributions",
     message:
       "Please include all contributors and contribution guidelines to include GitHub profiles and all third-party assets.",
       validate: function (input) {
         return input !== '' ? true : 'Contribution cannot be empty';
-      },  
-    name: "contributions",
+      },
   },
   {
     type: "input",
+    name: "tests",
     message:
       "Provide examples on how to run your test code and the expected results.",
       validate: function (input) {
         return input !== '' ? true : 'Test cannot be empty';
       },
-    name: "tests",
   },
   {
     type: "input",
+    name: "username",
     message: "Please enter your GitHub username",
     validate: function (input) {
       return input !== '' ? true : 'Must provide username';
     },
-    name: "username",
   },
   {
     type: "input",
+    name: "email",
     message: "Please enter your email address",
     validate: function (input) {
       return input !== '' ? true : 'Email cannot be empty';
     },
-    name: "email",
   },
-]);
+];
 
 function writeToFile(answers) {
   // Construct the README content in markdown using the user's inputs
@@ -142,7 +144,6 @@ function writeToFile(answers) {
     }
   });
 }
-
 
 // TODO: Create a function to write README file
 inquirer.prompt(questions).then((answers) => {
